469d790b0b1b5c2984732524079ee2b4f38fa524
tarungautam137 <tarungautam137@gmail.com>
2025-07-06 00:19:09 +0530

    http server changes

--- Server/app.js
+++ Server/app.js
@@ -1,5 +1,11 @@
 const http=require('http')
-const server=http.createServer();
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Tic-Tac-Toe Server Running');
+});
+
 require('dotenv').config()
 
 const socket=require('socket.io')
