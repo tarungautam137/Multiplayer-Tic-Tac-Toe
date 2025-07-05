ee0ae19855c98a05a911b3483e3a48d5cd7fcf36
tarungautam137 <tarungautam137@gmail.com>
2025-07-05 23:52:14 +0530

    BASE_URL changes

--- Client/src/constant.js
+++ Client/src/constant.js
@@ -1,5 +1,5 @@
 
-let BASE_URL="";
+let BASE_URL="https://multiplayer-tic-tac-toe-1-fgfx.onrender.com";
 if(location.hostname==='localhost') BASE_URL="http://localhost:3000";
 
 export default BASE_URL;
